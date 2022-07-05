27c324a68785b194c70e4c6cd83bd4faab312be4
Lucas do Nascimento Tavares <Ltv2234@gmail.com>
2022-07-05 15:51:16 -0300

    Fix: ajsute Layout tela de listagem

---
+++ Migrations/20220705184016_Adcionando_coluna_valor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    public partial class Adcionando_coluna_valor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Valor",
+                table: "Produtos",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Valor",
+                table: "Produtos");
+        }
+    }
+}
